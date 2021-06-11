c6c396ad8276287268c811879402d2acdd357a25
its_sanjay06 <sanjaykumar01.gh@gmail.com>
2021-06-11 14:54:00 +0530

    cms page changes

--- pages/cms/_cmsPage.vue
+++ pages/cms/_cmsPage.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    <vue-tabs
-      active-tab-color="#e74c3c"
-      active-text-color="white"
-      type="pills"
-      :start-index="1"
-      direction="vertical"
-    >
-      <v-tab v-for="(item, index) in cmsData" :key="index" :title="item.name">
-        <span v-html="item.content"></span>
-      </v-tab>
-    </vue-tabs>
+  <div class="cms_page ">
+    <div class="cms_page_content">
+      <vue-tabs
+        active-tab-color="#e74c3c"
+        active-text-color="white"
+        type="pills"
+        :start-index="1"
+        direction="vertical"
+      >
+        <v-tab v-for="(item, index) in cmsData" :key="index" :title="item.name">
+          <span v-html="item.content"></span>
+        </v-tab>
+      </vue-tabs>
+    </div>
+
+    <!-- <div class="containers">
+      <div class="top-search">
+        <h4 class="help_headline">Help</h4>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-12">
+          <ul>
+            <li
+              v-for="(item, index) in cmsData"
+              :key="index"
+              :title="item.name"
+            >
+              <a href="">{{ item.name }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-9 col-12">
+          <div v-for="(item, index) in cmsData" :key="index" :title="item.name">
+            <span v-html="item.content"></span>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="container">
+      <Contentasset />
+    </div>
   </div>
 </template>
 <script>
@@ -21,11 +51,11 @@ export default {
   components: {
     Contentasset,
     VueTabs,
-    VTab,
+    VTab
   },
   data() {
     return {
-      cmsData: {},
+      cmsData: {}
     };
   },
 
@@ -42,12 +72,15 @@ export default {
       let cmsData = await this.$store.dispatch("pimAjax", {
         method: "get",
         url: `/pimresponse.php`,
-        params: form,
+        params: form
       });
       if (cmsData.response.success) {
         this.cmsData = cmsData.result;
       }
     }
-  },
+  }
 };
 </script>
+<style scoped>
+@import url("@/assets/css/pages-css/cms.css");
+</style>
