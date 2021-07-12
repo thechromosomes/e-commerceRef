010bc818c8e96471157a5fbc9c21428dc33b27d6
sagar_gh <sagarkumar01.gh@gmail.com>
2021-07-12 14:22:15 +0530

    api key

--- plugins/thirdPartyModules.js
+++ plugins/thirdPartyModules.js
@@ -18,7 +18,8 @@ Vue.use(SimpleVueValidation);
 import * as VueGoogleMaps from "vue2-google-maps";
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyB_w48EfCO6yaYhPsrbKiQ-8yBUcJVTUpw",
+    // key: "AIzaSyB_w48EfCO6yaYhPsrbKiQ-8yBUcJVTUpw",
+    key: "AIzaSyADIZXv1w1AVnkacfh3AzL1VZLMgFWqTCE",
     libraries: "places,geometry",
   },
 });
